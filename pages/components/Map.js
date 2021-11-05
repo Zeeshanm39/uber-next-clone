3c0614a6e9300003e3f67c3911c3bd5cff2d2a7d
Zeeshan <zeeshanm39@gmail.com>
2021-11-06 00:35:48 +0530

    first commit

---
+++ pages/components/Map.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useEffect } from 'react';
+import tw from "tailwind-styled-components";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiemVlc2hhbm0iLCJhIjoiY2t2bWNjZzh3OWY2MjJvczdzem5mbXZqbiJ9.aAdtM9gh-nIf6VoWICYtJA";
+
+
+const Map = () => {
+
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+          container: "map",
+          style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+          center: [-99.29011,39.39172],
+          zoom: 3,
+        });
+      });
+
+    return (
+        <Wrapper id='map'>
+            
+        </Wrapper>
+    )
+}
+
+export default Map
+
+
+const Wrapper = tw.div`
+flex-1
+
+`
+
+
+
+
